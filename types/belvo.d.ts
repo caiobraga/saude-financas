e5951933bd295012ece16f34c69c4f6d8789f238
caiobraga <caiotagb@gmail.com>
2026-03-05 12:26:20 -0300

    fix error

--- types/belvo.d.ts
+++ types/belvo.d.ts
@@ -1,9 +1,29 @@
 declare module "belvo" {
+  interface LinksResource {
+    detail(linkId: string): Promise<unknown>;
+  }
+
+  interface AccountsResource {
+    retrieve(linkId: string): Promise<unknown[]>;
+  }
+
+  interface TransactionsResource {
+    retrieve(
+      linkId: string,
+      dateFrom: string,
+      options: { dateTo: string }
+    ): Promise<unknown[]>;
+  }
+
   export default class Client {
     constructor(
       secretKey: string,
       secretPassword: string,
       env: "sandbox" | "production"
     );
+    connect(): Promise<void>;
+    links: LinksResource;
+    accounts: AccountsResource;
+    transactions: TransactionsResource;
   }
 }
